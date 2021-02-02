d585240ce9ab9f5951f2cbe368c3c9373beb63de
Desmondezo1 <desezo@gmail.com>
2021-02-02 10:30:47 +0100

    corrected an error

--- src/app/search-result/product-results/product-item/product-item.component.ts
+++ src/app/search-result/product-results/product-item/product-item.component.ts
@@ -8,7 +8,7 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductItemComponent implements OnInit {
 
-  @Input() product: Product;
+  @Input() product: any;
 
   constructor() { }
 
