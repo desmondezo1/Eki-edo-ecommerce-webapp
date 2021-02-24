a54a27d1416c4a54e176076a3177b392d28240ef
Desmondezo1 <desezo@gmail.com>
2021-02-24 14:07:11 +0100

    added slider image from api

--- src/app/home/homebanner/carousel/carousel.component.ts
+++ src/app/home/homebanner/carousel/carousel.component.ts
@@ -1,5 +1,10 @@
+
+
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @Component({
   selector: 'app-carousel',
@@ -9,18 +14,53 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 })
 export class CarouselComponent implements OnInit {
 
+  // url = 'http://admin.ekiedong.com/wp-content/uploads/slide31.jpg';
   showNavigationArrows = false;
   showNavigationIndicators = false;
-  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor(config: NgbCarouselConfig) {
+  images = ['http://admin.ekiedong.com/wp-content/uploads/slide31.jpg','http://admin.ekiedong.com/wp-content/uploads/slide2.jpg','http://admin.ekiedong.com/wp-content/uploads/slide3.jpg'];
+
+  constructor(config: NgbCarouselConfig, private http: HttpClient) {
     // customize default values of carousels used by this component tree
     config.showNavigationArrows = true;
     config.showNavigationIndicators = true;
   }
 
-  ngOnInit():void{
+  setDefaultImage(){
+    console.log('error');
+  }
 
+
+  //  checkEachUrl(){
+  //   this.images.forEach(url => {
+
+  //     if(this.checkUrlifValid(url)){
+  //       console.log(this.checkUrlifValid(url))
+  //       console.log(true)
+  //     }else{
+  //       console.log(false);
+  //     }
+  //   //  fetch(url).then(res => {
+  //   //    console.log('boie-> ',res.ok);
+  //   //  });
+
+    // });
+  // }
+
+
+
+
+  // checkUrlifValid(url): any{
+  //   this.http.get(url).toPromise().then(a =>{ return true}).catch( error => { return false});
+
+  // }
+
+
+
+  ngOnInit():void{
+    // this.checkUrlifValid();
+    // this.checkEachUrl();
   }
 
 }
